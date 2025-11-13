351ca906d7f027368c76a2ee892a62de39a91010
vijitverm1234 <vijitverma78@gmailcom>
2025-11-13 23:26:33 +0530

    new file

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,12 +3,11 @@ const router = express.Router();
 const { signup, login } = require('../controllers/userController.js');
 const authenticate = require('../middleware/auth.js'); 
 const User = require('../models/userModel.js'); 
-
 router.post("/signup", signup);
 router.post("/login", login);
-
 router.get("/me", authenticate, async (req, res) =>{
   const user = await User.findById(req.user.id).select("-password");
   res.json(user);
 });
+
 module.exports = router;
